1d0e0f78cc64c6e7224fb59e65f5684493920ef6
Marc Erdmann <me@onlinefuels.de>
2023-01-18 17:06:48 +0100

    fix: version of axios

--- generators/package-json/index.ts
+++ generators/package-json/index.ts
@@ -33,7 +33,7 @@ export async function generate() {
     },
     dependencies: {
       agentkeepalive: '^4.2.1',
-      axios: '^1.1.2',
+      axios: '1.1.2',
       'browser-or-node': '^2.0.0',
       lodash: '^4.17.21',
       qs: '^6.11.0',
